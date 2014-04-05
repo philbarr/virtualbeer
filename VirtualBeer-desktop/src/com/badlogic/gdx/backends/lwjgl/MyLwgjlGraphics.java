6fd7abd419341e5efd916aae1caf17733ab1e429
philbarr <philbarr@e3ae36dc-4eb9-473b-9873-63a2b9b57ce8>
2014-04-05 16:02:04 +0000

    instructions

---
+++ VirtualBeer-desktop/src/com/badlogic/gdx/backends/lwjgl/MyLwgjlGraphics.java
@@ -0,0 +1,11 @@
+package com.badlogic.gdx.backends.lwjgl;
+
+public  class MyLwgjlGraphics extends LwjglGraphics
+{
+
+  public MyLwgjlGraphics(LwjglApplicationConfiguration config)
+  {
+    super(config);
+  }
+  
+}
